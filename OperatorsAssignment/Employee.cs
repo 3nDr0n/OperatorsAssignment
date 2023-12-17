1d5ffcf12cb9fa396cfb900f88b9a50387d4c765
Alexander Magyar <magyarali@gmail.com>
2023-12-17 14:43:09 +0000

    Add project files.

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+// cerating Employee class
+public class Employee
+{
+    // Properties
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Constructor
+    public Employee(int id, string firstName, string lastName)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    // Overloading the equality operator (==) to compare Employee objects based on their Id property
+    public static bool operator ==(Employee employee1, Employee employee2)
+    {
+        // Check if both objects are null or if their Id properties are equal
+        return ReferenceEquals(employee1, null) && ReferenceEquals(employee2, null) ||
+               !ReferenceEquals(employee1, null) && !ReferenceEquals(employee2, null) &&
+               employee1.Id == employee2.Id;
+    }
+
+    // Overloading the inequality operator
+    public static bool operator !=(Employee employee1, Employee employee2)
+    {
+        return !(employee1 == employee2);
+    }
+}
---
+++ OperatorsAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //creating employee objects
+        Employee employee1 = new Employee(1, "Mike", "Kovacs");
+        Employee employee2 = new Employee(2, "Lajos", "Szabo");
+        Employee employee3 = new Employee(1, "Mike", "Kovacs");
+
+        // Comparing employees using the overloaded equality operator
+        bool areEqual = employee1 == employee2;
+        Console.WriteLine($"Employee 1 and Employee 2 are equal: {areEqual}");
+
+        areEqual = employee1 == employee3;
+        Console.WriteLine($"Employee 1 and Employee 3 are equal: {areEqual}");
+        Console.ReadLine();
+    }
+}
